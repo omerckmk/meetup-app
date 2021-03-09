9d00e69a6cdab1d6010d52cc98efb4d3c47d4c2c
omerckmk <ckmkomr@icloud.com>
2021-03-09 22:57:26 +0200

    created project vue + vuetify, added navbar

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <navBar/>
+    <v-main>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import navBar from "./components/navBar";
+
+export default {
+  name: "App",
+
+  components: {
+    navBar
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>

1eacc59cca241b553c314d7709db17b8b9455a72
omerckmk <ckmkomr@icloud.com>
2021-03-10 02:05:29 +0200

    deleted navbar component, Meetup and user components added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,76 @@
 <template>
   <v-app>
-    <navBar/>
-    <v-main>
+    <v-toolbar dark class="primary">
+      <v-app-bar-nav-icon
+          @click.native.stop="showNav = !showNav"
+          class="hidden-sm-and-up"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">Dev Meetup</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+            class="text-decoration-none"
+            text
+            v-for="item in menuItems"
+            :key="item.title"
+            router
+            :to="item.link"
+        >
 
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer
+        temporary
+        v-model="showNav"
+    >
+      <v-list>
+        <v-list-item v-for="item in menuItems"
+                     :key="item.title"
+                     router
+                     :to="item.link"
+                     class="text-decoration-none"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import navBar from "./components/navBar";
+
 
 export default {
   name: "App",
+  components: {},
+  data() {
+    return {
+      showNav: false,
+      menuItems: [
+        {icon: 'mdi-account-supervisor', title: 'View Meetups', link: '/meetups'},
+        {icon: 'mdi-map-marker', title: 'Organize Meetup', link: '/meetups/new'},
+        {icon: 'mdi-account', title: 'Profile', link: '/profile'},
+        {icon: 'mdi-face', title: 'Sign Up', link: '/signup'},
+        {icon: 'mdi-lock-open', title: 'Sign in', link: '/signin'},
+      ]
 
-  components: {
-    navBar
+    }
   },
 
-  data: () => ({
-    //
-  })
 };
 </script>
+<style lang="sass">
+@import '../node_modules/vuetify/src/styles/main'
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,52 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Meetups from "@/components/Meetup/Meetups";
+import CreateMeetup from "@/components/Meetup/CreateMeetup";
+import Profile from "@/components/User/Profile";
+import Signup from "@/components/User/Signup";
+import Signin from "@/components/User/Signin";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  }
-  /*  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/!* webpackChunkName: "about" *!/ "../views/About.vue")
-  }*/
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/meetups",
+        name: "Meetups",
+        component: Meetups
+    },
+    {
+        path: "/profile",
+        name: "Profile",
+        component: Profile
+    },
+    {
+        path: "/signup",
+        name: "SignUp",
+        component: Signup
+    },
+    {
+        path: "/signin",
+        name: "Signin",
+        component: Signin
+    },
+    {
+        path: "/meetups/new",
+        name: "CreateMeetup",
+        component: CreateMeetup
+    }
+
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;

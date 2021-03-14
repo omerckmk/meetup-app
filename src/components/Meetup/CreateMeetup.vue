0be2415fe63463ac56a8b8035964912f3b1a996e
omerckmk <ckmkomr@icloud.com>
2021-03-14 02:09:45 +0200

    create meetup page(form)added and vee-validate added

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,12 +1,86 @@
 <template>
-<div>
-  <p>The create meetup Page</p>
-</div>
+  <v-container>
+    <v-row>
+      <v-col class="xs12 col-sm-6 offset-sm-3  ">
+        <h2>Create new Meetup</h2>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="xs12">
+        <form>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-3">
+              <v-text-field
+                  v-model="title"
+                  name="title"
+                  label="Title"
+                  id="title"
+                  required>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-3">
+              <v-text-field
+                  v-model="location"
+                  name="location"
+                  label="Location"
+                  id="location"
+                  required>
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-3">
+              <v-text-field
+                  v-model="imageUrl"
+                  name="imageUrl"
+                  label="İmage URL"
+                  id="image-url"
+                  required
+                  >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-3">
+              <v-textarea
+                  v-model="description"
+                  name="description"
+                  label="Description"
+                  id="description"
+                  required
+              >
+              </v-textarea>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-3">
+              <img :src=imageUrl alt="">
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="xs12 col-sm-6 offset-sm-7">
+              <v-btn class="primary">Create Meetup</v-btn>
+            </v-col>
+          </v-row>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "CreateMeetup"
+  name: "CreateMeetup",
+  data() {
+    return {
+      title: '',
+      location: '',
+      imageUrl : '',
+      description : ''
+    }
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 import vuetify from "./plugins/vuetify";
+
+import { extend, localize } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+import en from 'vee-validate/dist/locale/en.json';
+
+// install rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+localize('en', en);
+
 
 Vue.config.productionTip = false;
 
